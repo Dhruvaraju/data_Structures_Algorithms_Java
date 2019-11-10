f1c05f545b331e2e16bec369deb7e332ad9ff6df
dhruvaraju <49dhruva@gmail.com>
2019-11-10 12:18:49 +0530

    commit 01

--- example_projects/src/com/dsaa/ArrayOps.java
+++ example_projects/src/com/dsaa/ArrayOps.java
@@ -1,4 +1,15 @@
 package com.dsaa;
 
 public class ArrayOps {
+    public void createAndLogIntArray(int[] input){
+        for(int i = 0; i < input.length; i++){
+            input[i] = i+1;
+        }
+        if(null != input && input.length > 0){
+            //Logging data to console
+            for(int i=0; i < input.length; i++){
+                System.out.println("Value of array element "+ i +" is : "+ input[i]);
+            }
+        }
+    }
 }
---
+++ example_projects/src/com/dsaa/Main.java
@@ -0,0 +1,10 @@
+package com.dsaa;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayOps opp1 = new ArrayOps();
+        opp1.createAndLogIntArray(new int[10]);
+    }
+}
